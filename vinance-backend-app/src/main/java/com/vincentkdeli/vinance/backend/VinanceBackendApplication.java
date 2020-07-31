d829ea937f9e3eb5f6f5cb379fd7a8eb79a9a8b1
Vincent K. Deli <vincentkdeli@gmail.com>
2020-07-31 23:36:11 +0700

    Initial commit

---
+++ vinance-backend-app/src/main/java/com/vincentkdeli/vinance/backend/VinanceBackendApplication.java
@@ -0,0 +1,16 @@
+package com.vincentkdeli.vinance.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author - Vincent K. Deli
+ * @created - 31/07/20
+ */
+
+@SpringBootApplication
+public class VinanceBackendApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(VinanceBackendApplication.class, args);
+  }
+}
