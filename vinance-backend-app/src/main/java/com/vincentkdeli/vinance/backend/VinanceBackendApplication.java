2f9d9c27611954f251acef92582b5f4f48d83737
Vincent K. Deli <vincentkdeli@gmail.com>
2020-08-15 11:53:08 +0700

    added docker compose file, setup mongo properties

--- vinance-backend-app/src/main/java/com/vincentkdeli/vinance/backend/VinanceBackendApplication.java
+++ vinance-backend-app/src/main/java/com/vincentkdeli/vinance/backend/VinanceBackendApplication.java
@@ -2,12 +2,14 @@ package com.vincentkdeli.vinance.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * @author - Vincent K. Deli
  * @created - 31/07/20
  */
 
+@ConfigurationPropertiesScan({"com.vincentkdeli.vinance.backend.properties"})
 @SpringBootApplication
 public class VinanceBackendApplication {
 
